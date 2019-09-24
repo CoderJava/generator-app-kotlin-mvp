eb77b582ea002b1a6b206155f674d79c08cd5483
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2019-09-24 10:35:11 +0700

    Create function generator project

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 - clone it with `git clone --recursive git-url`
 - install dependencies `npm install`
 - link the project with `npm link`
-- run the generator `yo basekotlinandroid`
+- run the generator `yo base-kotlin-mvp`
---
+++ generators/app/templates/index.js
@@ -0,0 +1,102 @@
+const Generator = require('yeoman-generator');
+const mkdirp = require('mkdirp');
+const yosay = require('yosay');
+const chalk = require('chalk');
+
+module.exports = class extends Generator {
+
+
+    constructor(args, opts) {
+        super(args,opts);
+        this.option('babel')
+    }
+
+    initialize() {
+        this.props = {};
+
+        this.log(yosay(
+            'Welcome to ' + chalk.red('Generator Android')
+        ));
+
+        const prompts = [
+            {
+                name: 'Name',
+                message: 'What are you calling your app?',
+                store: true,
+                default: this.appname
+            },
+            {
+                name: 'package',
+                message: 'What package will you be publishing the app under?',
+                default: 'com.ysn.' + this.appname
+            },
+            {
+                name: 'targetSdk',
+                message: 'What Android SDK will you be targeting?',
+                store: true,
+                default: 29 // Android 9
+            },
+            {
+                name: 'minSdk',
+                message: 'What is the minimum Android SdK you wish to support?',
+                store: true,
+                default: 21 // Android 5.0
+            }
+        ];
+
+        return this.prompt(prompts).then(props => {
+            this.props.appPackage = props.package;
+            this.appName = props.name;
+            this.appPackage = props.package;
+            this.androidTargetSdkVersion = props.targetSdk;
+            this.androidMinSdkVersion = props.minSdk;
+        });
+    }
+
+    writingFunction() {
+        var packageDir = ths.props.appPackage.replace(/\./g, '/');
+        var appFolder = 'base-kotlin-mvp';
+
+        mkdirp('app');
+        mkdirp('app/src/main/java/' + packageDir);
+        mkdirp('app/src/androidTest/java/' + packageDir);
+        mkdirp('app/src/test/java/' + packageDir);
+
+        var appPath = this.sourceRoot() + '/' + appFolder + '/';
+
+        this.log("app path " + appPath);
+
+        const copyDotFile = filePath => {
+            const index = filePath.lastIndexOf('/') + 1;
+            const dest = filePath.substring(0, index) + '.${filePath.substring(filePath.lastIndexOf('/') + 1)}';
+
+            console.log('dest', dest);
+            console.log('origin', (appPath + filePath));
+
+            this.fs.copy(appPath + filePath, dest);
+        }
+
+        // copyDotFile('gitignore');
+        // copyDotFile('app/gitignore');
+
+        this.fs.copy(appPath + '.gitignore', '.gitignore');
+        this.fs.copy(appPath + 'app/.gitignore', 'app/.gitignore');
+        this.fs.copy(appPath + 'build.gradle', 'build.gradle');
+        this.fs.copy(appPath + 'gradle.properties', 'gradle.properties');
+        this.fs.copy(appPath + 'gradlew', 'gradlew');
+        this.fs.copy(appPath + 'gradlew.bat', 'gradlew.bat');
+        this.fs.copy(appPath + 'settings.gradle', 'settings.gradle');
+        this.fs.copy(appPath + 'versions.gradle', 'versions.gradle');
+        this.fs.copy(appPath + 'app/proguard-rules.pro', 'app/proguard-rules.pro');
+
+        this.fs.copy(appPath + 'gradle', 'gradle');
+        this.fs.copy(appPath + 'app/src/main/res', 'app/src/main/res');
+
+        this.fs.copyTpl(appPath + 'app/build.gradle', 'app/build.gradle', this.props);
+        this.fs.copyTpl(appPath + 'app/src/androidTest/java/com/ysn/basekotlinmvp', 'app/src/androidTest/java/' + packageDir, this.props);
+        this.fs.copyTpl(appPath + 'app/src/main/AndroidManifest.xml', 'app/src/main/AndroidManifest.xml', this.props);
+        this.fs.copyTpl(appPath + 'app/src/main/java/com/ysn/basekotlinmvp', 'app/src/main/java/' + packageDir, this.props);
+        this.fs.copyTpl(appPath + 'app/src/main/res/layout/', 'app/src/main/res/layout', this.props);
+        this.fs.copyTpl(appPath + 'app/src/test/java/com/ysn/basekotlinmvp', 'app/src/test/java/' + packageDir, this.props);
+    }
+};
